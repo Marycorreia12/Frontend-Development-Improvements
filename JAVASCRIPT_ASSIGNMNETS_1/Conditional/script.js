c59e6a33675fc0942a2129044100bf0220046e65
Maria Santos <correia_nety@hotmail.com>
2022-08-31 15:46:44 +0100

    Conditional Statement & Loops2

--- JAVASCRIPT_ASSIGNMNETS_1/Conditional/script.js
+++ JAVASCRIPT_ASSIGNMNETS_1/Conditional/script.js
@@ -1,16 +1,25 @@
 /* Write a JavaScript program that accepts two integers and displays the larger of the two */
 function largerNumber(num1, num2){
     if(num1 > num2){
-        console.log(`${num1} is larger than ${num2}`);
-        return num1;
+        return `${num1} is larger than ${num2}.`;
     }
     else if(num1 < num2){
-        console.log(`${num2} is larger than ${num1}`);
-        return num2;
+        return `${num2} is larger than ${num1}.`;
     }
     else if(num1 === num2)
-        console.log(`${num1} is equal to ${num2}`);
-        return num1 || num2;
+        return `${num1} is equal to ${num2}.`;
 }
 
-console.log(largerNumber(1, 1));
+console.log(largerNumber(1, 7));
+
+/* Write a JavaScript program that checks whether an integer is an even or an odd number.
+ */
+
+function evenOrOdd(num3){
+    if(num3 % 2 === 0)
+    return `${num3} is Even`;
+    else
+    return `${num3} is Odd`;
+}
+
+console.log(evenOrOdd(10));
