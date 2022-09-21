7cd36fc7c08f9e839d7a952940e73226693e3b68
Maria Santos <correia_nety@hotmail.com>
2022-09-21 12:25:24 +0100

    Home page accordion

---
+++ TASK_MANAGEMENT/js/home.js
@@ -0,0 +1,25 @@
+const accordion = document.querySelectorAll('li');
+
+accordion.forEach(item => {
+    item.addEventListener('click', () =>{
+        item.classList.toggle('text-show');
+
+        const text = item.firstElementChild;
+        if (text.style.display === "block") {
+            text.style.display = "none";
+            item.style.background = "#FFFFFF";
+            item.style.color = "#5B5555";
+          } else {
+            text.style.display = "block";
+            item.style.background = "#7568FE";
+            item.style.color = "#FFFFFF";
+          }
+
+          if (text.style.maxHeight) {
+            text.style.maxHeight = null;
+          } else {
+            text.style.maxHeight = text.scrollHeight + "px";
+          } 
+        console.log(text);
+    })
+})
